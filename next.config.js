33539c6054ba8957f57c55f01efedb546e3f999e
elianiva <dicha.arkana03@gmail.com>
2023-01-07 09:14:21 +0700

    refactor: proper key and use next image

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
     );
     return config;
   },
+  images: {
+    domains: ["avatars.githubusercontent.com", "media.licdn.com"],
+  },
   experimental: {
     appDir: true,
   },
--- parts/HomePage/ContributorSection.tsx
+++ parts/HomePage/ContributorSection.tsx
@@ -1,33 +1,49 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 type Contributor = {
-  name: string,
-  avatarUrl: string,
-  githubUrl: string,
-  jobDesc: string,
-}
+  name: string;
+  avatarUrl: string;
+  githubUrl: string;
+  jobDesc: string;
+};
 
 type ContributorSectionProps = {
-  contributors: Contributor[],
-}
+  contributors: Contributor[];
+};
 
-const ContributorSection: React.FC<ContributorSectionProps> = ({contributors}) => {
+const ContributorSection: React.FC<ContributorSectionProps> = ({
+  contributors,
+}) => {
   return (
     <section className={"py-px flex flex-col justify-center items-center"}>
-      <div className={"flex justify-center items-center gap-4 flex-col md:flex-col"}>
-        <h1 className={"text-slate-500 text-center font-semibold"}>
-          Build by
-        </h1>
+      <div
+        className={
+          "flex justify-center items-center gap-4 flex-col md:flex-col"
+        }
+      >
+        <h1 className={"text-slate-500 text-center font-semibold"}>Build by</h1>
         <div className={"flex-row gap-6 flex flex-wrap justify-center"}>
-          {contributors.map((contributors, index) => (
-            <a href={contributors.githubUrl} target={"_SEJ"} rel={"noopener"}>
+          {contributors.map((contributor) => (
+            <a
+              key={contributor.name}
+              href={contributor.githubUrl}
+              target="_SEJ"
+              rel="noopener"
+            >
               <div className={"text-white flex gap-3 items-center"}>
                 <div className={"bg-slate-300 w-fit p-1 rounded-full"}>
-                  <img className={"min-w-[2rem]s max-w-[2.5rem] rounded-full"} src={contributors.avatarUrl} />
+                  <Image
+                    width={32}
+                    height={32}
+                    className="min-w-[2rem]s max-w-[2.5rem] rounded-full"
+                    src={contributor.avatarUrl}
+                    alt={contributor.name}
+                  />
                 </div>
                 <div>
-                  <h4 className={"font-bold text-md"}>{contributors.name}</h4>
-                  <p className={"text-xs italic"}>{contributors.jobDesc}</p>
+                  <h4 className={"font-bold text-md"}>{contributor.name}</h4>
+                  <p className={"text-xs italic"}>{contributor.jobDesc}</p>
                 </div>
               </div>
             </a>
@@ -35,10 +51,12 @@ const ContributorSection: React.FC<ContributorSectionProps> = ({contributors}) =
         </div>
       </div>
       <div>
-        <a className={"font-regular text-xs text-slate-500"}>and other creative people... </a>
+        <a className={"font-regular text-xs text-slate-500"}>
+          and other creative people...{" "}
+        </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default ContributorSection;
