9262da0735bd80cd5042a405d150d8703234e313
Ali Zulfikar <62384197+alizul01@users.noreply.github.com>
2023-01-17 13:50:41 +0700

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin
+        redirectTo: 'https://*.vercel.app'
       }
     });
 
