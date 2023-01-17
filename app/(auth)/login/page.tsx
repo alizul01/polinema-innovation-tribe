9495a6d8c6166841ee19ab9937ce4d585dacd6d9
Ali Zulfikar <62384197+alizul01@users.noreply.github.com>
2023-01-17 13:46:59 +0700

    Fix: Redicert Supabase

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -18,6 +18,9 @@ export default function LoginPage() {
 
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: window.location.origin
+      }
     });
 
     if (error !== null) {
--- components/InputForm.tsx
+++ components/InputForm.tsx
@@ -12,7 +12,7 @@ export function InputForm (props: InputType) {
             <div className={"absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"}>
               {props.icon}
             </div>
-          ) : ''
+          ) : ' '
         }
         <input {...props.register} defaultValue={props.defaultValue} id={props.id} type={props.type} placeholder={props.placeholder} className={["border text-sm rounded-lg block w-full bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-purple-500 p-3 focus:border-purple-500", props.icon ? 'pl-10' : ' '].join(' ')} />
       </div>
