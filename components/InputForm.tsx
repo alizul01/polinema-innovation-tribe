229683a8aab67152e44e382556a983e113a77426
Ali Zulfikar <62384197+alizul01@users.noreply.github.com>
2023-01-17 12:08:19 +0700

    Fix Non Fullish ESLINT

--- components/InputForm.tsx
+++ components/InputForm.tsx
@@ -8,13 +8,13 @@ export function InputForm (props: InputType) {
       </label>
       <div className={"relative"}>
         {
-          props?.icon ? (
+          props.icon ? (
             <div className={"absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"}>
-              {props?.icon}
+              {props.icon}
             </div>
           ) : ''
         }
-        <input {...props.register} defaultValue={props.defaultValue} id={props.id} type={props.type} placeholder={props.placeholder} className={["border text-sm rounded-lg block w-full bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-purple-500 p-3 focus:border-purple-500", props?.icon ? 'pl-10' : ' '].join(' ')} />
+        <input {...props.register} defaultValue={props.defaultValue} id={props.id} type={props.type} placeholder={props.placeholder} className={["border text-sm rounded-lg block w-full bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-purple-500 p-3 focus:border-purple-500", props.icon ? 'pl-10' : ' '].join(' ')} />
       </div>
     </div>
   )
