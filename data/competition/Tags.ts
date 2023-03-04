751d73dbf7e2ccbab507f9b2722420211f26bb46
alizul01 <alizulfikar032@gmail.com>
2023-03-04 19:50:03 +0700

    fix type error

--- data/competition/Tags.ts
+++ data/competition/Tags.ts
@@ -2,57 +2,57 @@ import type { Tag } from '../../types/Idea/Index/Tag';
 
 const TAGS: Tag[] = [
   {
-    text: "Puspresnas",
+    name: "Puspresnas",
     slug: "puspresnas",
     colour: "rgba(249, 228, 212, 0.5)",
   },
   {
-    text: "Poster",
+    name: "Poster",
     slug: "poster",
     colour: "rgba(228, 241, 249, 0.5)",
   },
   {
-    text: "Videography",
+    name: "Videography",
     slug: "videography",
     colour: "rgba(212, 249, 232, 0.5)",
   },
   {
-    text: "Essay",
+    name: "Essay",
     slug: "essay",
     colour: "rgba(214, 232, 249, 0.5)",
   },
   {
-    text: "Coding",
+    name: "Coding",
     slug: "coding",
     colour: "rgba(249, 232, 214, 0.5)",
   },
   {
-    text: "Design",
+    name: "Design",
     slug: "design",
     colour: "rgba(232, 214, 249, 0.5)",
   },
   {
-    text: "UI / UX",
+    name: "UI / UX",
     slug: "ui-ux",
     colour: "rgba(214, 249, 228, 0.5)",
   },
   {
-    text: "Photography",
+    name: "Photography",
     slug: "photography",
     colour: "rgba(249, 214, 227, 0.5)",
   },
   {
-    text: "Music",
+    name: "Music",
     slug: "music",
     colour: "rgba(228, 212, 249, 0.5)",
   },
   {
-    text: "Scientific Research",
+    name: "Scientific Research",
     slug: "scientific-research",
     colour: "rgba(232, 249, 214, 0.5)",
   },
   {
-    text: "Business Plan",
+    name: "Business Plan",
     slug: "business-plan",
     colour: "rgba(214, 227, 249, 0.5)",
   }
