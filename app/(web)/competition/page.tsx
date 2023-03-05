d3ebb7ee906513e0ef8ee58886fcf6502ffcec55
alizul01 <alizulfikar032@gmail.com>
2023-03-05 18:17:48 +0700

    update page.tsx, Competition.tsx, CompetitionList.tsx and send-suggestion.ts

--- app/(web)/competition/page.tsx
+++ app/(web)/competition/page.tsx
@@ -1,11 +1,24 @@
-"use client"
+"use client";
 import COMPETITION from "~/data/competition/Competition";
 import TAGS from "~/data/competition/Tags";
 import CompetitionList from "~/parts/Competition/CompetitionList";
-import Header from "~/parts/Competition/Header"
-import TagList from '../../../parts/Idea/Index/TagList';
+import Header from "~/parts/Competition/Header";
+import TagList from "~/parts/Idea/Index/TagList";
+import { selectCompetition } from "~/services/competition/select-competition";
+import { useEffect } from "react";
+import { useSupabase } from "../../../components/Supabase/SupabaseProvider";
 
-export default function CompetitionPage() {
+export default  function CompetitionPage() {
+  const { supabase } = useSupabase();
+  useEffect(() => {
+    async function fetchCompetition() {
+      const { data, error } = await supabase.from("competitions").select("*");
+      console.log(data, error);
+    }
+
+    fetchCompetition().then((r) => console.log(r)).catch((e) => console.log(e));
+  }, [supabase]);
+  
   return (
     <div>
       <section className="w-full h-full pt-1 md:pt-8 relative">
